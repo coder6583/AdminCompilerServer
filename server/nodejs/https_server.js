aa78ffb41959d87c1dea663c01552b4867101d10
curvingchicken <thedude6583@gmail.com>
2021-04-03 19:36:49 +0900

    test

--- server/nodejs/https_server.js
+++ server/nodejs/https_server.js
@@ -130,7 +130,7 @@ app.use(passport_1.default.initialize());
 app.use(passport_1.default.session());
 app.use(everyRequest);
 function everyRequest(req, res, next) {
-    console.log(req.session);
+    console.log(req);
     if (req.user != "admin" && req.url != '/login' || req.url != '/') {
         res.sendFile('index.html', { root: rootdirectory });
         console.log('not logged in');
--- server/nodejs/https_server.ts
+++ server/nodejs/https_server.ts
@@ -149,7 +149,7 @@ app.use(everyRequest);
 
 function everyRequest(req: express.Request, res: express.Response, next: express.NextFunction)
 {
-  console.log(req.session);
+  console.log(req);
     if(req.user != "admin" && req.url != '/login' || req.url != '/')
     {
       res.sendFile('index.html', {root: rootdirectory});
