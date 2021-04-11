cbc261f3f3b13d42ae1981904e44286c458a857a
mario-hsp <47262658+mario-hsp@users.noreply.github.com>
2021-04-11 22:01:17 +0900

    escape fix

--- client/js/admin.ts
+++ client/js/admin.ts
@@ -419,7 +419,10 @@ const servers :{[key:string]:string} = {
 function parseServerLog(logs :serverLog[]) {
 	const resolveCategory = (category :string) => categorys[category] || '';
 	const resolveServer = (server :string) => servers[server] || '';
-	const escapeLog = (log :string) => log.replace('<', '&lt;').replace('>', '&gt;');
+	const escapeLog = (log :string) => {
+		console.log(log.replace('<', '&lt;').replace('>', '&gt;'));
+		return log.replace('<', '&lt;').replace('>', '&gt;')
+	};
 	logs.forEach(log => {
 		$('#server-log > tbody').append(`<tr><td class="${log.server}">${resolveServer(log.server)}</td><td class="${log.category}">${resolveCategory(log.category)}</td><td>${escapeLog(log.value)}</td><td>${moment(new Date(log.timestamp)).format('YYYY/MM/DD HH:mm:ss')}</td></tr>`);
 	});
