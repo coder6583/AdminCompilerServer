20fb10ae1165757745f23c1c6f7efe168ed47744
mario-hsp <47262658+mario-hsp@users.noreply.github.com>
2021-04-23 17:27:37 +0900

    blacklist add

--- client/js/admin.js
+++ client/js/admin.js
@@ -250,7 +250,28 @@ $(function () {
     });
     // banIP
     $('#add-ban-ip').on('submit', function () {
-        var banIP = $('#ban-ip-box').val();
+        var _a;
+        var banIP = (_a = $('#ban-ip-box').val()) === null || _a === void 0 ? void 0 : _a.toString();
+        if (!banIP)
+            return false;
+        if (banIP.match(/\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}/)) {
+            // IPv4
+            socket.emit('blacklistAdd', {
+                value: "::ffff:" + banIP
+            });
+            $('#ban-ip-box').val('');
+        }
+        else if (banIP.match(/(([0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,7}:|([0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,5}(:[0-9a-fA-F]{1,4}){1,2}|([0-9a-fA-F]{1,4}:){1,4}(:[0-9a-fA-F]{1,4}){1,3}|([0-9a-fA-F]{1,4}:){1,3}(:[0-9a-fA-F]{1,4}){1,4}|([0-9a-fA-F]{1,4}:){1,2}(:[0-9a-fA-F]{1,4}){1,5}|[0-9a-fA-F]{1,4}:((:[0-9a-fA-F]{1,4}){1,6})|:((:[0-9a-fA-F]{1,4}){1,7}|:)|fe80:(:[0-9a-fA-F]{0,4}){0,4}%[0-9a-zA-Z]{1,}|::(ffff(:0{1,4}){0,1}:){0,1}((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])|([0-9a-fA-F]{1,4}:){1,4}:((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9]))/)) {
+            // IPv6
+            socket.emit('blacklistAdd', {
+                value: banIP
+            });
+            $('#ban-ip-box').val('');
+        }
+        else {
+            popupMessage('IPアドレスの形式が有効ではありません', 'err');
+        }
+        return false;
     });
     // モニター
     var chartData = {
