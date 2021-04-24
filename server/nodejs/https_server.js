b03dbebab2bd6fc3c386051625a31f88d4501153
curvingchicken <thedude6583@gmail.com>
2021-04-24 13:23:35 +0900

    log filter log

--- server/nodejs/https_server.js
+++ server/nodejs/https_server.js
@@ -203,6 +203,7 @@ io.sockets.on('connection', function (socket) { return __awaiter(void 0, void 0,
         socket.on('logGet', function (input) { return __awaiter(void 0, void 0, void 0, function () {
             var serverFilter, filteredLog, jsonLogs;
             return __generator(this, function (_a) {
+                functions.LOG(input, 'log filter');
                 serverFilter = functions.parseServerFilter(input.filter.server);
                 filteredLog = [];
                 jsonLogs = [];
